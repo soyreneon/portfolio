520ae6c1f491a7bfd777748dd320723f2ef2ebe2
Rene Limon Chacon <renecomes@gmail.com>
2026-04-04 18:03:59 -0600

    fix error when accesing subpaths on github domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import path from "path";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +12,13 @@ export default defineConfig({
     // alias: {
     //   "@": path.resolve(__dirname, "./src"),
     // },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "public/404.html"),
+      },
+    },
   },
 });
